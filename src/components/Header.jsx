b7862eafff028e2f1dcd7140147eedf2bf3dbc0e
NGOVINDARAJU <158595722+NGOVINDARAJU@users.noreply.github.com>
2025-08-28 01:01:13 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Header() {
 return (
 <header style={{ background: '#222', color: 'white', padding: '10px 20px' }}>
-<h1>Demo React CI/CD App</h1>
+<h1>🚀 CI/CD Verified — Live!</h1>
 </header>
 );
 }
