bf066f638bb78cab5f9d2cb69c07038674dcf981
NGOVINDARAJU <158595722+NGOVINDARAJU@users.noreply.github.com>
2025-08-30 18:55:12 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,8 @@ import React from 'react';
 export default function Header() {
 return (
 <header style={{ background: '#222', color: 'white', padding: '10px 20px' }}>
-<h1>CI/CD Verified — Live!</h1>
+<h1>Stitching Snadhya papa</h1>
   <h2>N.Govinda Raju</h2>
-  <h1>Loves</h1>
   <h2>Sandhya</h2>
 </header>
 );
