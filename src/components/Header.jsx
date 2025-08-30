a66689894b1dbdb4a25615c057ca8f074dd2f793
NGOVINDARAJU <158595722+NGOVINDARAJU@users.noreply.github.com>
2025-08-30 18:50:28 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,8 @@ return (
 <header style={{ background: '#222', color: 'white', padding: '10px 20px' }}>
 <h1>CI/CD Verified — Live!</h1>
   <h2>N.Govinda Raju</h2>
+  <h1>Loves</h1>
+  <h2>Sandhya</h2>
 </header>
 );
 }
